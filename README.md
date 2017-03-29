898ff7ad8c40981fd0d956e3134bb1b97d61e60a
fresh5447 <doug@kosmojo.com>
2017-03-28 21:29:07 -0600

    add express so we can create a web server

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 It's nice to update our .gitignore before our first commit..
+
+
+had to run npm init to make a package.json => every node proj has one of these
+
+had to install express so we can use it as our dev server
+
+created the server on port 3000
+
+create an endpoint /test to handle a GET request..
--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+var express = require('express');
+var app     = express();
+
+
+// app.METHOD('URL LOCATION', function(req, res){ handle req/res })
+
+// 'have to go to localhost:3000/test to find this..'
+app.get('/test', function(req, res){
+  res.send("You found the test route");
+});
+
+var server = app.listen(3000, function(){
+  console.log('Server running 🔥🔥 on PORT 3000');
+});
